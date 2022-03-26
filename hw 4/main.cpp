2f99311ed635e8803c77c9f224da69fe7404481a
Safa Mohammed <sumohammed0@gmail.com>
2022-03-26 15:27:36 -0500

    Initial Commit

---
+++ hw 4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw 4
+//
+//  Created by Safa Mohammed on 3/26/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
